7cc3e1b2f394400955d55a147e35f4ecc4088a37
boyna <boyna1212@mail.ru>
2026-06-28 20:35:19 +0300

    new func mask_account_number

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from src.masks import get_mask_card_number
+
+
+def mask_account_card(number_account: str) -> str:
+    """Функция возврщает строку с замаскированным номером"""
+    if not isinstance(number_account, str):
+        raise ValueError("Данные должны быть строкой")
+    if not number_account:
+        raise ValueError("Строка не может быть пуста")
+    parts = number_account.split()
+    number_card = parts[-1]
+    type_card = " ".join(parts[:-1])
+    if not number_card.isdigit():
+        raise ValueError("Номер карты должен состоять только из цифр")
+    try:
+        if type_card.lower().startswith("счет"):
+            mask_card = mask_account_card(number_card)
+        else:
+            mask_card = get_mask_card_number(number_card)
+        return f"{type_card} {mask_card}".strip()
+    except ValueError as e:
+        raise ValueError(f"Ошибка маскирования {e}")
+
