57bf03db5cde8b73f21311216b7d815ab21a90a5
boyna <boyna1212@mail.ru>
2026-06-28 13:36:57 +0300

    Described the tests for the get_mask_number_card func

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,24 +1,23 @@
 import pytest
 from src.masks import get_mask_card_number
 
-
-def test_returns_get_mask_card_number(test_name):
+# Test for the get_mask_card_number func
+def test_returns_get_mask_card_number(test_name): # Тест чтобы понять, что возвращает функция
     return test_name
 
-def test_valid_get_mask_card_number(test_name):
+def test_valid_get_mask_card_number(test_name): # Тест с валидными данными
     assert get_mask_card_number(test_name) == "** 1111"
 
-
-def test_str_null_get_mask_card_number():
+def test_str_null_get_mask_card_number(): # Тест обработки ошибки, если пользователь вводит пробел или пустую строку
     with pytest.raises(ValueError):
         get_mask_card_number(" ")
 
-
-def test_invalid_small_number_get_mask_card_number(test_small_number_card):
+def test_invalid_small_number_get_mask_card_number(test_small_number_card): # Тест обработки ошибки, если пользователь вводит короткий номер карты
     with pytest.raises(ValueError):
         get_mask_card_number(test_small_number_card)
 
-
-def test_sym_and_num_get_mask_card_number(test_num_and_sym_number_card):
+def test_sym_and_num_get_mask_card_number(test_num_and_sym_number_card): # Тест обработки ошибки, если пользователь вводит номер с цифрами и буквами
     with pytest.raises(ValueError):
         get_mask_card_number(test_num_and_sym_number_card)
+
+# test for the get_mask_account func
