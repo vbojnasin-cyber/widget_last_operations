90412909f711e8b9550476309643aede568dab69
boyna <boyna1212@mail.ru>
2026-07-01 20:25:18 +0300

    update func filter_by_currency, transaction_descriptions, number_card_generator

--- src/generators.py
+++ src/generators.py
@@ -1,25 +1,44 @@
-from typing import List, Dict
-def filter_by_currency(data_list: List[Dict], currency: str) -> List[Dict]:
+from typing import Dict, Iterator, List
+
+
+def filter_by_currency(data_list: List[Dict], currency: str) -> Iterator[Dict]:
     """Фильтрует список транзакций по указанному коду валюты."""
-    if not data_list or not currency:
+    if not data_list and not currency:
         raise ValueError("Вы ничего не передали")
-    # Проверяю, что передан именно список
+
     if not isinstance(data_list, list):
         raise ValueError("Ошибка передачи данных, вы передали не список")
-    # Проверяю, что вcе элементы внутри словари
+
     for data in data_list:
         if not isinstance(data, dict):
             raise ValueError("Ошибка передачи данных")
-    # Фильтрация транзакции (благодаря .get() она полностью безопасна)
-    return [
-        transaction for transaction in data_list
-        if transaction.get('operationAmount', {}).get('currency', {}).get('code') == currency
-    ]
-def transaction_descriptions(transactions: List[dict]) -> List[str]:
+
+        if data.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+            yield data
+
+
+def transaction_descriptions(transactions: List[dict]) -> Iterator[Dict]:
+    """Функция фильтрует транзакции по описанию переводов"""
     if not transactions:
         raise ValueError("Передали пустую транзакцию")
+
     if not isinstance(transactions, list):
         raise ValueError("Ошибка передачи данных, передали не тот тип")
-    """Функция фильтрует транзакции по описанию переводов"""
-    return [transaction["description"] for transaction in transactions if transaction.get("description")]
 
+    for transaction in transactions:
+        if not isinstance(transaction, dict):
+            raise ValueError("Ошибка данных, передали не слоаврь")
+
+        if transaction.get("description", ""):
+            yield transaction["description"]
+
+
+def number_card_generator(start: int, end: int) -> Iterator[str]:
+    if start < 1 or end > 9999999999999999 or start > end:
+        raise ValueError("Неправильно заданы значения")
+    for number in range(start, end + 1):
+        card_str = f"{number:016d}"
+        card_number = (
+            f"{card_str[0:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:16]}"
+        )
+        yield card_number
