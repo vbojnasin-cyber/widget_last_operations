5e3266d46aeed4163bb6c30cd71d590e4d417c29
boyna <boyna1212@mail.ru>
2026-07-03 09:19:10 +0300

    update mask.py

--- src/masks.py
+++ src/masks.py
@@ -1,26 +1,23 @@
 def get_mask_card_number(number_card: str) -> str:
     """Функция которая маскирует номер карты"""
     if number_card == " " or number_card == "":
-        raise ValueError("Номер карты не может состоять из пустой строки")
-    clean_number_card = number_card.replace(" ", "")  # Убираем пробелы
+        raise ValueError(f"Номер карты не может состоять из пустой строки, вы ввели - {number_card}")
+    clean_number_card = number_card.replace(" ", "")
     if not clean_number_card.isdigit():
-        raise ValueError(f"Номер карты должен состоять только из цифр")
-    if len(clean_number_card) != 16:  # Проверяем на валидность номер карты
+        raise ValueError(f"Номер карты должен состоять только из цифр, номер который вы ввели - {clean_number_card}")
+    if len(clean_number_card) != 16:
         raise ValueError(
-            f"Номер карты должен содержать 16 цифр,"
-            f" ваш содержит - {len(clean_number_card)}"
-        )  # Обрабатываем ошибку
-
-    return f"**** **** **** {clean_number_card[-4:]}"  # Вывод номера карты с маской
+            f"Номер карты должен содержать 16 цифр, ваш содержит - {len(clean_number_card)}")
+    return f"**** **** **** {clean_number_card[-4:]}"
 
 
 def get_mask_account(number_account: str) -> str:
     """Функция, которая маскирует номер счета"""
     if number_account == " " or number_account == "":
-        raise ValueError("Номер счета не может состоять из пустой строки")
+        raise ValueError(f"Номер счета не может состоять из пустой строки, вы ввели - {number_account}")
     clean_number_account = number_account.replace(" ", "")
     if not clean_number_account.isdigit():
-        raise ValueError("Номер счета должен состоять только из цифр")
+        raise ValueError(f"Номер счета должен состоять только из цифр, номер который вы ввели - {clean_number_account}")
     if len(clean_number_account) == 20:
         return f"** {clean_number_account[-4:]}"
     else:
