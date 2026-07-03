ee49c35922ee8978279cf64a96b0e3a24a07ea70
boyna <boyna1212@mail.ru>
2026-07-03 18:55:14 +0300

    update test for the func

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,36 +1,40 @@
 import pytest
-
 from src.widget import mask_account_card
 
 
-def test_return_mask_account_card():
-    assert (
-        mask_account_card("Visa Mastercard 1111111111111111")
-        == "Visa Mastercard **** **** **** 1111"
-    )
+# Test for the func mask_account_card
 
+# 1 test
+def test_valid_number_mask_account_card():
+    """Тест при валидных данных банковской карты"""
+    assert (mask_account_card("Visa Mastercard 1111111111111111") == "Visa Mastercard **** **** **** 1111")
 
-def test_sym_and_num_mask_account_card():
+# 2 test
+def test_valid_account_mask_account_card():
+    """Тест при валидных данных счета"""
+    assert mask_account_card("Счет 01111111111111111111") == "Счет ** 1111"
+
+# 3 test
+def test_letter_in_number_mask_account_card():
+    """Тест ошибки, если пользователь введет некорректный номер карты с буквами"""
     with pytest.raises(ValueError):
         mask_account_card("Visa Mastercard 112212fgf2231111")
 
+# 4 test
+def test_letter_in_account_mask_account_card():
+    """Тест ошибки, если пользователь введет некорректный номер счета с буквами"""
+    with pytest.raises(ValueError):
+        mask_account_card("Счет 011111ggggggggg11111")
 
+# 5 test
 def test_list_mask_account_card():
+    """Тест ошибки, если в аргумент передадут список"""
     with pytest.raises(ValueError):
         mask_account_card(["Visa", "Mastercard", "1233333333333333"])
 
-
-def test_long_num_mask_account_card():
+# 6 test
+def test_long_number_mask_account_card():
+    """Тест при длинном номере карты"""
     with pytest.raises(ValueError):
         mask_account_card("Visa Mastercard 11111111111111111")
 
-
-@pytest.mark.parametrize(
-    "number_card, expected",
-    [
-        ("Maestro 1596837868705199", "Maestro **** **** **** 5199"),
-        ("Счет 64686473678894779589", "Счет ** 9589"),
-    ],
-)
-def test_mask(number_card, expected):
-    assert mask_account_card(number_card) == expected
