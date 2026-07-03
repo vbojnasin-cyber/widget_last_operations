8a79d58b27fc16a6e53f58bb075c7a75c2a48b3a
boyna <boyna1212@mail.ru>
2026-07-03 18:55:04 +0300

    update func

--- src/widget.py
+++ src/widget.py
@@ -4,14 +4,14 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(number_account: str) -> str:
     """Функция возврщает строку с замаскированным номером"""
     if not isinstance(number_account, str):
-        raise ValueError("Данные должны быть строкой")
+        raise ValueError(f"Данные должны быть строкой, ваш тип данных - {type(number_account)}")
     if not number_account:
-        raise ValueError("Строка не может быть пуста")
+        raise ValueError(f"Строка не может быть пуста, вы ввели - {number_account}")
     parts = number_account.split()
     number_card = parts[-1]
     type_card = " ".join(parts[:-1])
     if not number_card.isdigit():
-        raise ValueError("Номер карты должен состоять только из цифр")
+        raise ValueError(f"Номер карты должен состоять только из цифр, ваш номер карты - {number_card}")
     try:
         if type_card.lower().startswith("счет"):
             mask_card = get_mask_account(number_card)
