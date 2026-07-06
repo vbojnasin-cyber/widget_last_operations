3cf272fd8dbf81d2fba0b99d7ba214c347947b23
boyna <boyna1212@mail.ru>
2026-07-06 22:43:19 +0300

    Final development of all core functions

--- src/masks.py
+++ src/masks.py
@@ -1,34 +1,45 @@
+import logging
+from pathlib import Path
+from src import loggers
+
+name = Path(__file__).stem
+file_name = f"{name}.log"
+logger = loggers.create_logger(name, file_name, logging.DEBUG)
+
 def get_mask_card_number(number_card: str) -> str:
     """Функция которая маскирует номер карты"""
     if number_card == " " or number_card == "":
-        raise ValueError(
-            f"Номер карты не может состоять из пустой строки, вы ввели - {number_card}"
-        )
+        error_msg = f"Номер карты не может быть пустым, вы ввели: '{number_card}'"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
     clean_number_card = number_card.replace(" ", "")
     if not clean_number_card.isdigit():
-        raise ValueError(
-            f"Номер карты должен состоять только из цифр, номер который вы ввели - {clean_number_card}"
-        )
+        error_msg = f"Номер карты должен состоять только из цифр, вы ввели: '{clean_number_card}'"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
     if len(clean_number_card) != 16:
-        raise ValueError(
-            f"Номер карты должен содержать 16 цифр, ваш содержит - {len(clean_number_card)}"
-        )
-    return f"**** **** **** {clean_number_card[-4:]}"
+        error_msg = f"Номер карты должен содержать 16 цифр, ваш содержит: {len(clean_number_card)}"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
 
+    logger.info("Номер карты успешно замаскирован")
+    return f"**** **** **** {clean_number_card[-4:]}"
 
 def get_mask_account(number_account: str) -> str:
     """Функция, которая маскирует номер счета"""
     if number_account == " " or number_account == "":
-        raise ValueError(
-            f"Номер счета не может состоять из пустой строки, вы ввели - {number_account}"
-        )
+        error_msg = f"Номер счета не может быть пустым, вы ввели: '{number_account}'"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
     clean_number_account = number_account.replace(" ", "")
     if not clean_number_account.isdigit():
-        raise ValueError(
-            f"Номер счета должен состоять только из цифр, номер который вы ввели - {clean_number_account}"
-        )
+        error_msg = f"Номер счета должен состоять только из цифр, вы ввели: '{clean_number_account}'"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
     if len(clean_number_account) != 20:
-        raise ValueError(
-            f"Номер счета должен состоять из 20 цифр, ваш состоит из {len(clean_number_account)}"
-        )
+        error_msg = f"Номер счета должен состоять из 20 цифр, ваш состоит из {len(clean_number_account)}"
+        logger.error(error_msg)
+        raise ValueError(error_msg)
+
+    logger.info("Номер счета успешно замаскирован")
     return f"** {clean_number_account[-4:]}"
